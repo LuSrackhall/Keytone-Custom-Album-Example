dee7d4612ecb779201edf4a069dc12e33c70d0f6
LuSrackhall <3647637206@qq.com>
2025-04-05 06:20:44 +0800

    新增mechvibes中multi格式键音包转换为keyTone中album的脚本, 并通过mechvibes中的默认示例做了验证延时, 目前由于脚本中没有按键时长, 故无法轻易分类down和up的声音, 因此本次新增的转换脚本仅简单转换成仅支持down声音的album

---
+++ mechvibes/multi-only-down/mechvibesMulti-keytoneAlbum~formatConversion.js
@@ -0,0 +1,383 @@
+// // 第一组数据 - 键值映射
+// const sourceMapping = {
+//   1: "q.wav",
+//   2: "w.wav",
+//   // ... 用户需要在这里填入完整的第一组数据
+// };
+
+// // 第二组数据 - 音频文件信息
+// const audioFiles = {
+//   "073ed3b4d33193972829a45af65cf7861ab9a96b765b2cb34e24cd2858c66f11": {
+//     name: {
+//       0: "d",
+//     },
+//     type: ".wav",
+//   },
+//   // ... 用户需要在这里填入完整的第二组数据
+// };
+
+const sourceMapping = {
+  1: "q.wav",
+  2: "w.wav",
+  3: "e.wav",
+  4: "e.wav",
+  5: "r.wav",
+  6: "t.wav",
+  7: "y.wav",
+  8: "u.wav",
+  9: "i.wav",
+  10: "o.wav",
+  11: "p.wav",
+  12: "[.wav",
+  13: "].wav",
+  14: "backspace.wav",
+  15: "tab.wav",
+  16: "q.wav",
+  17: "w.wav",
+  18: "e.wav",
+  19: "r.wav",
+  20: "t.wav",
+  21: "y.wav",
+  22: "u.wav",
+  23: "i.wav",
+  24: "o.wav",
+  25: "p.wav",
+  26: "[.wav",
+  27: "].wav",
+  28: "enter.wav",
+  29: "tab.wav",
+  30: "a.wav",
+  31: "s.wav",
+  32: "d.wav",
+  33: "f.wav",
+  34: "g.wav",
+  35: "h.wav",
+  36: "j.wav",
+  37: "k.wav",
+  38: "l.wav",
+  39: "[.wav",
+  40: "].wav",
+  41: "q.wav",
+  42: "shift.wav",
+  43: "backspace.wav",
+  44: "z.wav",
+  45: "x.wav",
+  46: "c.wav",
+  47: "v.wav",
+  48: "b.wav",
+  49: "n.wav",
+  50: "m.wav",
+  51: "l.wav",
+  52: "[.wav",
+  53: "].wav",
+  54: "shift.wav",
+  55: "o.wav",
+  56: "e.wav",
+  57: "space.wav",
+  58: "caps lock.wav",
+  59: "w.wav",
+  60: "e.wav",
+  61: "e.wav",
+  62: "r.wav",
+  63: "t.wav",
+  64: "y.wav",
+  65: "u.wav",
+  66: "i.wav",
+  67: "o.wav",
+  68: "p.wav",
+  69: "u.wav",
+  70: "i.wav",
+  71: "y.wav",
+  72: "u.wav",
+  73: "i.wav",
+  74: "p.wav",
+  75: "h.wav",
+  76: "j.wav",
+  77: "k.wav",
+  78: "enter.wav",
+  79: "b.wav",
+  80: "n.wav",
+  81: "m.wav",
+  82: "shift.wav",
+  83: "[.wav",
+  87: "].wav",
+  88: "].wav",
+  91: null,
+  92: null,
+  93: null,
+  3597: null,
+  3612: "shift.wav",
+  3613: "backspace.wav",
+  3637: "i.wav",
+  3639: "u.wav",
+  3640: "p.wav",
+  3653: "o.wav",
+  3655: "h.wav",
+  3657: "k.wav",
+  3663: "n.wav",
+  3665: "m.wav",
+  3666: "h.wav",
+  3667: "b.wav",
+  3675: "q.wav",
+  3676: "[.wav",
+  3677: "].wav",
+  57416: "g.wav",
+  57419: "c.wav",
+  57421: "b.wav",
+  57424: "v.wav",
+  60999: "h.wav",
+  61000: "g.wav",
+  61001: "k.wav",
+  61003: "c.wav",
+  61005: "b.wav",
+  61007: "n.wav",
+  61008: "v.wav",
+  61009: "m.wav",
+  61010: "h.wav",
+  61011: "b.wav",
+};
+
+const audioFiles = {
+  "073ed3b4d33193972829a45af65cf7861ab9a96b765b2cb34e24cd2858c66f11": {
+    name: {
+      0: "d",
+    },
+    type: ".wav",
+  },
+  "16f6aed02a5f607652beb1fabeb2db889c940286e5b8f9680e976335f63c4d4d": {
+    name: {
+      0: "a",
+    },
+    type: ".wav",
+  },
+  "1de19d4848ac5cf95bfd490581b76c4083ed2f3f2c83dad69c87e195292f2649": {
+    name: {
+      0: "k",
+    },
+    type: ".wav",
+  },
+  "2d2895bb975d89d7db1b3e1c2fccc4e7a23c426f26257e6fdc1532eb5107de0f": {
+    name: {
+      0: "c",
+    },
+    type: ".wav",
+  },
+  "2ffd4b3efe7a0704ee9baf8d05831f3f9ce49b245f2519bd98c33a0f9ef87fd8": {
+    name: {
+      0: "s",
+    },
+    type: ".wav",
+  },
+  "343408e5ce18eb1b2f1b62831399a28d7d13c60fc798e9b7ce3a6ad52efc93ba": {
+    name: {
+      0: "h",
+    },
+    type: ".wav",
+  },
+  "448ccf397763890c08f8cc00c09fd1b733aa19a7dbd46b8ece5673b9caaabac0": {
+    name: {
+      0: "n",
+    },
+    type: ".wav",
+  },
+  "4865ac5e114d1c4be035a8ffb8b4b8c85db8e6813221aa8a961613e7f20bd6ff": {
+    name: {
+      0: "f",
+    },
+    type: ".wav",
+  },
+  "48a3e2ca3fd5e6a115124da6066877e9b223fa57429b4a769cfb8d4f3cb47e78": {
+    name: {
+      0: "v",
+    },
+    type: ".wav",
+  },
+  "4ae50700b87ff29456f5177c52d7f386a2d757aa96ed21e6c4d6f8cfbcfd8a0f": {
+    name: {
+      0: "z",
+    },
+    type: ".wav",
+  },
+  "4b167168476e42f7585258dc0e77e87f719ded427970ea4a708df18dc711fd2e": {
+    name: {
+      0: "x",
+    },
+    type: ".wav",
+  },
+  "4b6a1dadee30ad98ae50353a6e3be233eb6f44828bb853394b099605d6a6106a": {
+    name: {
+      0: "y",
+    },
+    type: ".wav",
+  },
+  "4dd1489fef18d52373d4a00786d4f03c9fea8569b4a1294d3c333da3b2b899c6": {
+    name: {
+      0: "g",
+    },
+    type: ".wav",
+  },
+  "5b243a471e2e7d294a6bf30c53417abb63edb1b1cc8c9b2c4d098c1bed169a53": {
+    name: {
+      0: "space",
+    },
+    type: ".wav",
+  },
+  "621980ab08b5f54391e718076c7d200455d54d022db9c275f24af87f6aba6c72": {
+    name: {
+      0: "e",
+    },
+    type: ".wav",
+  },
+  "9510a9a48607a8efce83b31ac938bc79dda3f95b5bdedc62c21a60b8ebdfcf36": {
+    name: {
+      0: "caps lock",
+      1: "tab",
+    },
+    type: ".wav",
+  },
+  "97309a4a865d6174727cc94dcf440e0f6714e3a9bce3afa40d5008f2f0747da9": {
+    name: {
+      0: "b",
+    },
+    type: ".wav",
+  },
+  "986b002df479032b77e0bf904e906b823a6b272b94d1058c329d5d1b1c33a0c5": {
+    name: {
+      0: "enter",
+      1: "shift",
+    },
+    type: ".wav",
+  },
+  "9c97bd0eddd161d3546b010f46487c39c38754a77c01cb7a2c4c5e4499eaa396": {
+    name: {
+      0: "j",
+    },
+    type: ".wav",
+  },
+  a5d75fabc7d6d53c8c2df91147ca43bec55d02efbdcf59e63a9a21157b75569e: {
+    name: {
+      0: "]",
+      1: "p",
+    },
+    type: ".wav",
+  },
+  a9e5cacd4d34aaa66fd17bc92380f2e6a65381286137969f754186813c7478be: {
+    name: {
+      0: "r",
+    },
+    type: ".wav",
+  },
+  afcb8635b3051eae3f02492fffbfb3d98bba25218550836e8d68c7879c43a7b5: {
+    name: {
+      0: "q",
+    },
+    type: ".wav",
+  },
+  b65cb53b0078476e5ef9508e69894bed65a38a2e451f0f21a2b4a4662020cc95: {
+    name: {
+      0: "l",
+    },
+    type: ".wav",
+  },
+  b7e53974136b221256efdf07ae8ef817c8abed6fad71955cdcfeb28e587a7db1: {
+    name: {
+      0: "w",
+    },
+    type: ".wav",
+  },
+  c521b053daeb2d390083c4b2455cb3f760371d5570339e76c2855c62ca598841: {
+    name: {
+      0: "m",
+    },
+    type: ".wav",
+  },
+  d1f30b7be773820e1ef106309972ac3c7222c6b3a5379a6d54d84638c9865329: {
+    name: {
+      0: "i",
+    },
+    type: ".wav",
+  },
+  d3f42a8b92555ca0db586b20ae6a321520dcbf73db6a41e6c3e06722f02431d8: {
+    name: {
+      0: "[",
+      1: "o",
+    },
+    type: ".wav",
+  },
+  d66128202910f5f972e76967160cc4e49bd3a3fc3972d8229932baea955f5e00: {
+    name: {
+      0: "t",
+    },
+    type: ".wav",
+  },
+  e9f1fe1c641487bff608cf3dca8b5acb8c75835163515e14517c60bb65d1b14f: {
+    name: {
+      0: "backspace",
+    },
+    type: ".wav",
+  },
+  f1a80d1973de3c5b0d72ca302006fd9dec14d380fe8fa0b1689394284676925a: {
+    name: {
+      0: "u",
+    },
+    type: ".wav",
+  },
+};
+
+// 创建反向查找映射
+function createReverseAudioMapping() {
+  const reverseMapping = {};
+
+  for (const [sha256, data] of Object.entries(audioFiles)) {
+    const names = Object.values(data.name);
+    names.forEach((name) => {
+      reverseMapping[name] = {
+        sha256,
+        type: data.type,
+        nameId: Object.keys(data.name).find((key) => data.name[key] === name),
+      };
+    });
+  }
+
+  return reverseMapping;
+}
+
+// 转换格式
+function convertFormat() {
+  const result = {};
+  const reverseMapping = createReverseAudioMapping();
+
+  for (const [key, value] of Object.entries(sourceMapping)) {
+    // 跳过空值
+    if (!value) continue;
+
+    // 获取纯文件名（去除.wav后缀）
+    const fileName = value.replace(".wav", "");
+
+    // 查找对应的音频文件信息
+    const audioInfo = reverseMapping[fileName];
+    if (!audioInfo) continue;
+
+    // 构建新的格式
+    result[key] = {
+      down: {
+        type: "audio_files",
+        value: {
+          name_id: audioInfo.nameId,
+          sha256: audioInfo.sha256,
+          type: audioInfo.type,
+        },
+      },
+      up: {
+        type: "",
+        value: "",
+      },
+    };
+  }
+
+  return result;
+}
+
+// 执行转换并输出结果
+const convertedData = convertFormat();
+console.log(JSON.stringify(convertedData, null, 2));
